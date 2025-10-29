d312b84bee8dc06f0c6996ef4059eabb0f37b657
Rail Alimov <ra.alimov@gmail.com>
2025-10-29 18:19:09 +0700

    Setup logging and add exceptions

--- app/main.py
+++ app/main.py
@@ -1,6 +1,7 @@
 import asyncio
 import os
 import hashlib
+import logging
 from datetime import datetime
 from enum import Enum
 from typing import Any
@@ -10,12 +11,34 @@ from uuid import UUID
 
 import asyncpg
 import base58
+import structlog
 from fastapi import FastAPI
 from fastapi import Depends
 from fastapi import HTTPException
 from pydantic import BaseModel
 from starlette.responses import Response
 from starlette.requests import Request
+
+
+logging.basicConfig(
+    level=logging.DEBUG,
+    handlers=[logging.StreamHandler()]
+)
+
+structlog.configure(
+    processors=[
+        structlog.processors.TimeStamper(fmt="iso"),
+        structlog.processors.StackInfoRenderer(),
+        structlog.processors.format_exc_info,
+        structlog.processors.JSONRenderer()
+    ],
+    logger_factory=structlog.stdlib.LoggerFactory(),
+    wrapper_class=structlog.stdlib.BoundLogger,
+    cache_logger_on_first_use=True
+)
+
+
+logger = structlog.get_logger()
 
 
 POSTGRES_URL = os.getenv("POSTGRES_URL")
@@ -59,28 +82,9 @@ class DashboardResponse(BaseModel):
     workers: list[Worker]
     agg: AggregatedStats
 
-"""
-Example
-{
-  "workers": [
-    {
-      "id": "uuid",
-      "name": "string",
-      "status": "online|offline|inactive",
-      "last_seen_at": "RFC3339Z",
-      "hashrate_th": "string with 3 decimals"
-    }
-  ],
-  "agg": {
-    "online": 0,
-    "offline": 0,
-    "inactive": 0,
-    "total_hashrate_th": "string with 3 decimals"
-  }
-}
-"""
 
 async def get_postgres_connect():
+    # TODO use connection pool
     conn = await asyncpg.connect(POSTGRES_URL)
     try:
         yield conn
@@ -93,17 +97,48 @@ async def public_dashboard(token: str, conn=Depends(get_postgres_connect)):
     return await fetch_data_by_token(conn, token)
 
 
-async def get_watcher_link_record(db_conn: asyncpg.Connection,  payload_hash: bytes) -> Optional[dict[str, Any]]:
-    return await db_conn.fetchrow(
-        """
-        SELECT * FROM watcher_links
-        WHERE
-            payload_hash = $1
-            AND revoked_at IS NULL
-            AND expires_at > now()
-        """,
-        payload_hash,
-    )
+async def fetch_data_by_token(db_conn: asyncpg.Connection, token: str):
+    payload_hash = decode_token(token)
+
+    watcher_link_record = await get_watcher_link_record(db_conn, payload_hash)
+
+    if not watcher_link_record:
+        raise InvalidToken()
+
+    user_id = watcher_link_record["user_id"]
+
+    workers_task = fetch_workers_by_user_id(db_conn, user_id)
+    agg_task = get_aggregate_stats(db_conn, user_id)
+
+    workers, agg = await asyncio.gather(workers_task, agg_task)
+
+    return DashboardResponse(workers=workers, agg=agg)
+
+
+def decode_token(token: str) -> bytes:
+    """
+    Формат: Base58Check (как в биткоине): payload(16 bytes) + checksum(4 bytes = first4(sha256(sha256(payload)))), целиком закодировано Base58.
+    """
+
+    try:
+        raw_payload = base58.b58decode(token)
+    except ValueError:
+        logger.debug("")
+        raise InvalidToken()
+
+    if len(raw_payload) != 20:  # 16 bytest(payload) + 4 bytest(checksum)
+        logger.debug("")
+        raise InvalidToken()
+
+    payload = raw_payload[:16]
+    checksum_from_token = raw_payload[16:]
+
+    checksum = hashlib.sha256(hashlib.sha256(payload).digest()).digest()[:4]
+    if checksum_from_token != checksum:
+        logger.debug("")
+        raise InvalidToken()
+
+    return hashlib.sha256(payload).digest()
 
 
 async def fetch_workers_by_user_id(db_conn: asyncpg.Connection, user_id: Union[UUID, str]) -> list[Worker]:
@@ -126,6 +161,29 @@ async def fetch_workers_by_user_id(db_conn: asyncpg.Connection, user_id: Union[U
     return workers
 
 
+async def get_aggregate_stats(db_conn: asyncpg.Connection, user_id: Union[UUID, str]) -> AggregatedStats:
+    agg_record = await aggregate_stats(db_conn, user_id)
+    return AggregatedStats(
+        online=agg_record["online"],
+        offline=agg_record["offline"],
+        inactive=agg_record["inactive"],
+        total_hashrate_th=str(agg_record["total_hashrate_th"]),
+    )
+
+
+async def get_watcher_link_record(db_conn: asyncpg.Connection,  payload_hash: bytes) -> Optional[dict[str, Any]]:
+    return await db_conn.fetchrow(
+        """
+        SELECT * FROM watcher_links
+        WHERE
+            payload_hash = $1
+            AND revoked_at IS NULL
+            AND expires_at > now()
+        """,
+        payload_hash,
+    )
+
+
 async def filter_workers_by_user_id(db_conn: asyncpg.Connection, user_id: Union[UUID, str]) -> list[dict[str, Any]]:
     return  await db_conn.fetch(
         """
@@ -135,16 +193,6 @@ async def filter_workers_by_user_id(db_conn: asyncpg.Connection, user_id: Union[
         ORDER BY last_seen_at DESC
         """,
         user_id,
-    )
-
-
-async def get_aggregate_stats(db_conn: asyncpg.Connection, user_id: Union[UUID, str]) -> AggregatedStats:
-    agg_record = await aggregate_stats(db_conn, user_id)
-    return AggregatedStats(
-        online=agg_record["online"],
-        offline=agg_record["offline"],
-        inactive=agg_record["inactive"],
-        total_hashrate_th=str(agg_record["total_hashrate_th"]),
     )
 
 
@@ -172,42 +220,16 @@ async def aggregate_stats(db_conn: asyncpg.Connection, user_id: Union[UUID, str]
     }
 
 
-async def fetch_data_by_token(db_conn: asyncpg.Connection, token: str):
-    payload_hash = decode_token(token)
+class APIException(HTTPException):
+    status_code: int
+    detail: str
 
-    watcher_link_record = await get_watcher_link_record(db_conn, payload_hash)
-
-    if not watcher_link_record:
-        raise HTTPException(status_code=404, detail="Not found or invalid token")
-
-    user_id = watcher_link_record["user_id"]
-
-    workers_task = fetch_workers_by_user_id(db_conn, user_id)
-    agg_task = get_aggregate_stats(db_conn, user_id)
-
-    workers, agg = await asyncio.gather(workers_task, agg_task)
-
-    return DashboardResponse(workers=workers, agg=agg)
+    def __init__(self, detail: Optional[str] = None):
+        if detail is not None:
+            self.detail = detail
+        super().__init__(self.status_code, self.detail)
 
 
-def decode_token(token: str) -> bytes:
-    """
-    Формат: Base58Check (как в биткоине): payload(16 bytes) + checksum(4 bytes = first4(sha256(sha256(payload)))), целиком закодировано Base58.
-    """
-
-    try:
-        raw_payload = base58.b58decode(token)
-    except ValueError:
-        return None  # TODO raise exception
-
-    if len(raw_payload) != 20:  # 16 bytest(payload) + 4 bytest(checksum)
-        return None
-
-    payload = raw_payload[:16]
-    checksum_from_token = raw_payload[16:]
-
-    checksum = hashlib.sha256(hashlib.sha256(payload).digest()).digest()[:4]
-    if checksum_from_token != checksum:
-        return None
-
-    return hashlib.sha256(payload).digest()
+class InvalidToken(APIException):
+    status_code = 404
+    detail = "Not found or invalid token"
